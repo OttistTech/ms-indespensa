92e236a07b5acb7bd0d96181b4be70ba79dcf904
pedroggwp <phsnnascimento@outlook.com>
2024-08-18 14:15:14 -0300

    UserRepository with method findByEmail

---
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ottistech.indespensa.api.ms_indespensa.repository;
+
+import com.ottistech.indespensa.api.ms_indespensa.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
