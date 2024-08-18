8048b2e2bc3a340d53cf0a4de73ea203f95feb68
pedroggwp <phsnnascimento@outlook.com>
2024-08-18 19:14:33 -0300

    endpoint that receives an user id and deactivate his account

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/controller/UserController.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/controller/UserController.java
@@ -8,10 +8,7 @@ import com.ottistech.indespensa.api.ms_indespensa.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -37,5 +34,13 @@ public class UserController {
         UserCredentialsResponse userCredentials = userService.getUserCredentials(loginUserDTO);
 
         return ResponseEntity.status(HttpStatus.OK).body(userCredentials);
+    }
+
+    @DeleteMapping("/deactivation/{id}")
+    public ResponseEntity<Void> deactivateUser(@PathVariable("id") Long userId) {
+
+        userService.deactivateUserById(userId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
