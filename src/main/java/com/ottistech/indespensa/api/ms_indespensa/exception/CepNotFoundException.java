d4d6dac191d9f47b5a4b2c8620f5951796b10d55
pedroggwp <phsnnascimento@outlook.com>
2024-08-21 11:08:15 -0300

    CEP exception in case when user doesn't have a CEP register yet

---
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/exception/CepNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ottistech.indespensa.api.ms_indespensa.exception;
+
+public class CepNotFoundException extends RuntimeException {
+
+    public CepNotFoundException(String message) {
+        super(message);
+    }
+}
