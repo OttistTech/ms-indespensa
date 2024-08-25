a439d416cc626a918411694a7d9f8b80b15ac311
pedroggwp <phsnnascimento@outlook.com>
2024-08-25 11:18:18 -0300

    adding field bool to know if the request was successful

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/dto/ProductResponseApiDTO.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/dto/ProductResponseApiDTO.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public record ProductResponseApiDTO(
         String title,
+        Boolean success,
         String alias,
         String description,
         String brand,
