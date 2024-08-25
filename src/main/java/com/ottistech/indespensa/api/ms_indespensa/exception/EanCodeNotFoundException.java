a765c2f586b8aed645c68e4a9b7f4295f6afd74a
pedroggwp <phsnnascimento@outlook.com>
2024-08-25 11:19:17 -0300

    exception thrown when its not found an barcode in our database and in the external api

---
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/exception/EanCodeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.ottistech.indespensa.api.ms_indespensa.exception;
+
+public class EanCodeNotFoundException extends RuntimeException {
+
+    public EanCodeNotFoundException(String message) {
+        super(message);
+    }
+}
