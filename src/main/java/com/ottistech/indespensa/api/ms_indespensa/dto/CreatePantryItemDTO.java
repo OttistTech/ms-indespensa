e365f2cc60e139b43b98206b89e4426821195a11
pedroggwp <phsnnascimento@outlook.com>
2024-08-25 18:16:42 -0300

    DTO requested when user wants to add a new item in his pantry

---
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/dto/CreatePantryItemDTO.java
@@ -0,0 +1,21 @@
+package com.ottistech.indespensa.api.ms_indespensa.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record CreatePantryItemDTO(
+        String productEanCode,
+        @NotNull(message = "Field productName is required") String productName,
+        @NotNull(message = "Field productDescription is required") String productDescription,
+        String productImageUrl,
+        @NotNull(message = "Field productAmount is required") BigDecimal productAmount,
+        @NotNull(message = "Field productUnit is required") String productUnit,
+        @NotNull(message = "Field foodName is required") String foodName,
+        @NotNull(message = "Field categoryName is required") String categoryName,
+        @NotNull(message = "Field brandName is required") String brandName,
+        @NotNull(message = "Field pantryAmount is required") Integer pantryAmount,
+        @NotNull(message = "Field validityDate is required") LocalDate validityDate
+) {
+}
