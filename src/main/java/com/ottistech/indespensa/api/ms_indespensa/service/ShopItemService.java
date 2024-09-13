d92e3a39a5c90312aa38ae5d6b3bc83a915bd53f
pedroggwp <phsnnascimento@outlook.com>
2024-09-13 15:54:33 -0300

    service that verifies if product is already in the shopping-list of user, if its then just sum the amount passed in the request, if it doesn't, just create it

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/service/ShopItemService.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/service/ShopItemService.java
@@ -1,8 +1,12 @@
 package com.ottistech.indespensa.api.ms_indespensa.service;
 
-import com.ottistech.indespensa.api.ms_indespensa.dto.response.ListShopItemResponseDTO;
+import com.ottistech.indespensa.api.ms_indespensa.dto.request.AddShopItemDTO;
+import com.ottistech.indespensa.api.ms_indespensa.dto.response.ShopItemResponseDTO;
 import com.ottistech.indespensa.api.ms_indespensa.exception.UserNotFoundException;
+import com.ottistech.indespensa.api.ms_indespensa.model.Product;
+import com.ottistech.indespensa.api.ms_indespensa.model.ShopItem;
 import com.ottistech.indespensa.api.ms_indespensa.model.User;
+import com.ottistech.indespensa.api.ms_indespensa.repository.ProductRepository;
 import com.ottistech.indespensa.api.ms_indespensa.repository.ShopItemRepository;
 import com.ottistech.indespensa.api.ms_indespensa.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -18,16 +22,38 @@ public class ShopItemService {
 
     private final UserRepository userRepository;
     private final ShopItemRepository shopItemRepository;
+    private final ProductRepository productRepository;
 
-    public List<ListShopItemResponseDTO> getListItem(Long userId) {
+    public List<ShopItemResponseDTO> getListItem(Long userId) {
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException("User does not exist"));
 
-        List<ListShopItemResponseDTO> listItemResponses = shopItemRepository.findAllByUser(user);
+        List<ShopItemResponseDTO> listItemResponses = shopItemRepository.findAllByUser(user);
 
         if (listItemResponses.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No items found for the user");
 
         return listItemResponses;
+    }
+
+    public ShopItemResponseDTO addShopItem(Long userId, AddShopItemDTO shopItemDTO) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User does not exist"));
+
+        Product product = productRepository.findById(shopItemDTO.productId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No product found for this id"));
+
+        ShopItem shopItem = shopItemRepository.findByUserAndProductWithNullPurchaseDate(user, product)
+                .orElse(null);
+
+        if (shopItem != null) {
+            shopItem.setAmount(shopItem.getAmount() + shopItemDTO.amount());
+        } else {
+            shopItem = shopItemDTO.toShopItem(user, product, shopItemDTO);
+        }
+
+        shopItemRepository.save(shopItem);
+
+        return shopItemDTO.toShopItemResponseDto(shopItem);
     }
 }
