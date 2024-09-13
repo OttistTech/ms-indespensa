6e230b23badb849a3f0950ce7923cafb6b63bf99
pedroggwp <phsnnascimento@outlook.com>
2024-09-13 15:53:01 -0300

    queries that return the products with the passed user and product entities

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/repository/ShopItemRepository.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/repository/ShopItemRepository.java
@@ -1,17 +1,20 @@
 package com.ottistech.indespensa.api.ms_indespensa.repository;
 
-import com.ottistech.indespensa.api.ms_indespensa.dto.response.ListShopItemResponseDTO;
+import com.ottistech.indespensa.api.ms_indespensa.dto.response.ShopItemResponseDTO;
+import com.ottistech.indespensa.api.ms_indespensa.model.Product;
 import com.ottistech.indespensa.api.ms_indespensa.model.ShopItem;
 import com.ottistech.indespensa.api.ms_indespensa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ShopItemRepository extends JpaRepository<ShopItem, Long> {
 
     @Query("""
-       SELECT new com.ottistech.indespensa.api.ms_indespensa.dto.response.ListShopItemResponseDTO(
+       SELECT new com.ottistech.indespensa.api.ms_indespensa.dto.response.ShopItemResponseDTO(
             li.listItemId,\s
             li.user.userId,\s
             f.foodName,\s
@@ -24,7 +27,17 @@ public interface ShopItemRepository extends JpaRepository<ShopItem, Long> {
        JOIN li.product p
        JOIN p.foodId f
        WHERE li.user = :user \s
-       AND li.purchaseDate IS NOT NULL
+       AND li.purchaseDate IS NULL
       \s""")
-    List<ListShopItemResponseDTO> findAllByUser(User user);
+    List<ShopItemResponseDTO> findAllByUser(User user);
+
+    @Query("""
+      SELECT
+        si \s
+      FROM ShopItem si \s
+      WHERE si.user = :user \s
+      AND si.product = :product \s
+      AND si.purchaseDate IS NULL
+      \s""")
+    Optional<ShopItem> findByUserAndProductWithNullPurchaseDate(User user, Product product);
 }
