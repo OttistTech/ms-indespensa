13f2079b99fc176345464a60ab043c4e4ab072b7
pedroggwp <phsnnascimento@outlook.com>
2024-09-13 13:07:47 -0300

    changing name from ListItem to ShopItem

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/service/ShopItemService.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/service/ShopItemService.java
@@ -1,9 +1,9 @@
 package com.ottistech.indespensa.api.ms_indespensa.service;
 
-import com.ottistech.indespensa.api.ms_indespensa.dto.response.ListItemResponseDTO;
+import com.ottistech.indespensa.api.ms_indespensa.dto.response.ListShopItemResponseDTO;
 import com.ottistech.indespensa.api.ms_indespensa.exception.UserNotFoundException;
 import com.ottistech.indespensa.api.ms_indespensa.model.User;
-import com.ottistech.indespensa.api.ms_indespensa.repository.ListItemRepository;
+import com.ottistech.indespensa.api.ms_indespensa.repository.ShopItemRepository;
 import com.ottistech.indespensa.api.ms_indespensa.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,17 +14,17 @@ import java.util.List;
 
 @AllArgsConstructor
 @Service
-public class ListItemService {
+public class ShopItemService {
 
     private final UserRepository userRepository;
-    private final ListItemRepository listItemRepository;
+    private final ShopItemRepository shopItemRepository;
 
-    public List<ListItemResponseDTO> getListItem(Long userId) {
+    public List<ListShopItemResponseDTO> getListItem(Long userId) {
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UserNotFoundException("User does not exist"));
 
-        List<ListItemResponseDTO> listItemResponses = listItemRepository.findAllByUser(user);
+        List<ListShopItemResponseDTO> listItemResponses = shopItemRepository.findAllByUser(user);
 
         if (listItemResponses.isEmpty()) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No items found for the user");
 
