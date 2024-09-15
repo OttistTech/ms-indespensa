55a78257e7df1373b275d45692e34cbde859f902
Davi Piassi Barros dos Santos <davibarros-ieg@instituto.ad>
2024-09-15 17:43:26 -0300

    preventing from duplicates

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/repository/PantryItemRepository.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/repository/PantryItemRepository.java
@@ -2,11 +2,14 @@ package com.ottistech.indespensa.api.ms_indespensa.repository;
 
 import com.ottistech.indespensa.api.ms_indespensa.dto.PartialPantryItemDTO;
 import com.ottistech.indespensa.api.ms_indespensa.model.PantryItem;
+import com.ottistech.indespensa.api.ms_indespensa.model.Product;
 import com.ottistech.indespensa.api.ms_indespensa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface PantryItemRepository extends JpaRepository<PantryItem, Long> {
     @Query("""
@@ -27,4 +30,14 @@ public interface PantryItemRepository extends JpaRepository<PantryItem, Long> {
             ORDER BY pi.validityDate
             """)
     List<PartialPantryItemDTO> findAllActiveItemsByUser(User user);
+
+    @Query("""
+        SELECT pi
+        FROM PantryItem pi
+        WHERE pi.product = :product
+        AND pi.user = :user
+        AND pi.validityDate = :validityDate
+        AND pi.isActive = true
+        """)
+    Optional<PantryItem> findExistentPantryItem(User user, Product product, LocalDate validityDate);
 }
