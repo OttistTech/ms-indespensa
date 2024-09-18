0adacb3047df3a81f943ad6955d287aa3b1fe7f8
pedroggwp <phsnnascimento@outlook.com>
2024-09-18 09:14:55 -0300

    using method in the dto

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/service/PantryItemService.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/service/PantryItemService.java
@@ -1,16 +1,19 @@
 package com.ottistech.indespensa.api.ms_indespensa.service;
 
-import com.ottistech.indespensa.api.ms_indespensa.dto.request.PantryItemCreateDTO;
-import com.ottistech.indespensa.api.ms_indespensa.dto.response.PantryItemSimplifiedResponseDTO;
-import com.ottistech.indespensa.api.ms_indespensa.dto.response.PantryItemPartialDTO;
+import com.ottistech.indespensa.api.ms_indespensa.dto.request.CreatePantryItemDTO;
 import com.ottistech.indespensa.api.ms_indespensa.dto.request.UpdatePantryItemDTO;
 import com.ottistech.indespensa.api.ms_indespensa.dto.response.PantryItemDetailsDTO;
-import com.ottistech.indespensa.api.ms_indespensa.exception.PantryItemNotFoundException;
-import com.ottistech.indespensa.api.ms_indespensa.exception.UserNotFoundException;
-import com.ottistech.indespensa.api.ms_indespensa.model.*;
-import com.ottistech.indespensa.api.ms_indespensa.repository.*;
+import com.ottistech.indespensa.api.ms_indespensa.dto.response.PantryItemPartialDTO;
+import com.ottistech.indespensa.api.ms_indespensa.dto.response.PantryItemSimplifiedResponseDTO;
+import com.ottistech.indespensa.api.ms_indespensa.model.PantryItem;
+import com.ottistech.indespensa.api.ms_indespensa.model.Product;
+import com.ottistech.indespensa.api.ms_indespensa.model.User;
+import com.ottistech.indespensa.api.ms_indespensa.repository.PantryItemRepository;
+import com.ottistech.indespensa.api.ms_indespensa.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -22,23 +25,21 @@ import java.util.Optional;
 public class PantryItemService {
 
     private final PantryItemRepository pantryItemRepository;
-    private final ProductRepository productRepository;
     private final UserRepository userRepository;
-    private final BrandRepository brandRepository;
-    private final CategoryRepository categoryRepository;
-    private final FoodRepository foodRepository;
+    private final ProductService productService;
 
-    public PantryItemSimplifiedResponseDTO createPantryItem(Long userId, PantryItemCreateDTO pantryItemDTO) {
-        Product product = getOrCreateProduct(pantryItemDTO);
+    public PantryItemSimplifiedResponseDTO createPantryItem(Long userId, CreatePantryItemDTO pantryItemDTO) {
+        Product product = productService.getOrCreateProduct(pantryItemDTO.toProductDto());
 
         User user = userRepository.findById(userId)
-            .orElseThrow(() -> new UserNotFoundException("User not found and can't add an item to his pantry"));
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found and can't add an item to his pantry"));
 
         Optional<PantryItem> pantryItemOptional = pantryItemRepository.findExistentPantryItem(user, product, pantryItemDTO.validityDate());
         PantryItem pantryItem;
 
         if(pantryItemOptional.isEmpty()) {
             pantryItem = new PantryItem();
+
             pantryItem.setUser(user);
             pantryItem.setProduct(product);
             pantryItem.setAmount(pantryItemDTO.pantryAmount());
@@ -51,90 +52,16 @@ public class PantryItemService {
 
         pantryItemRepository.save(pantryItem);
 
-        return new PantryItemSimplifiedResponseDTO(
-                pantryItem.getProduct().getEanCode(),
-                pantryItem.getProduct().getName(),
-                pantryItem.getProduct().getImageUrl(),
-                pantryItem.getProduct().getFoodId().getFoodName(),
-                pantryItem.getProduct().getCategoryId().getCategoryName(),
-                pantryItem.getProduct().getDescription(),
-                pantryItem.getProduct().getBrandId().getBrandName(),
-                pantryItem.getProduct().getAmount(),
-                pantryItem.getProduct().getUnit(),
-                pantryItem.getProduct().getType(),
-                pantryItem.getUser().getUserId(),
-                pantryItem.getAmount(),
-                pantryItem.getValidityDate(),
-                pantryItem.getPurchaseDate(),
-                pantryItem.getIsActive()
-        );
-    }
-
-    private Product getOrCreateProduct(PantryItemCreateDTO pantryItemDTO) {
-        Optional<Product> existingProductByEanCode = productRepository.findByEanCodeNotNull(pantryItemDTO.productEanCode());
-
-        if (existingProductByEanCode.isPresent()) {
-            return existingProductByEanCode.get();
-        }
-
-        Optional<Product> existingProductByName = productRepository.findByNameNotNull(pantryItemDTO.productName());
-
-        return existingProductByName.orElseGet(() -> createNewProduct(pantryItemDTO));
-    }
-
-    private Product createNewProduct(PantryItemCreateDTO pantryItemDTO) {
-        Product product = new Product();
-
-        product.setEanCode(pantryItemDTO.productEanCode());
-        product.setName(pantryItemDTO.productName());
-        product.setImageUrl(pantryItemDTO.productImageUrl());
-        product.setDescription(pantryItemDTO.productDescription());
-        product.setAmount(pantryItemDTO.productAmount());
-        product.setUnit(pantryItemDTO.productUnit());
-
-        product.setFoodId(getOrCreateFood(pantryItemDTO.foodName()));
-        product.setBrandId(getOrCreateBrand(pantryItemDTO.brandName()));
-        product.setCategoryId(getOrCreateCategory(pantryItemDTO.categoryName()));
-
-        productRepository.save(product);
-
-        return product;
-    }
-
-    private Food getOrCreateFood(String foodName) {
-        return foodRepository.findByFoodName(foodName)
-                .orElseGet(() -> {
-                    Food newFood = new Food();
-                    newFood.setFoodName(foodName);
-                    return foodRepository.save(newFood);
-                });
-    }
-
-    private Brand getOrCreateBrand(String brandName) {
-        return brandRepository.findByBrandName(brandName)
-                .orElseGet(() -> {
-                    Brand newBrand = new Brand();
-                    newBrand.setBrandName(brandName);
-                    return brandRepository.save(newBrand);
-                });
-    }
-
-    private Category getOrCreateCategory(String categoryName) {
-        return categoryRepository.findByCategoryName(categoryName)
-                .orElseGet(() -> {
-                    Category newCategory = new Category();
-                    newCategory.setCategoryName(categoryName);
-                    return categoryRepository.save(newCategory);
-                });
+        return PantryItemSimplifiedResponseDTO.fromPantryItem(pantryItem);
     }
 
     public List<PantryItemPartialDTO> listPantryItems(Long userId) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException("User does not exist")); 
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist"));
 
         List<PantryItemPartialDTO> userActivePantryItems = pantryItemRepository.findAllActiveItemsByUser(user);
         if(userActivePantryItems.isEmpty()) {
-            throw new PantryItemNotFoundException("No pantry items found for the given userId");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No pantry items found for the given userId");
         }
 
         return userActivePantryItems;
@@ -142,22 +69,28 @@ public class PantryItemService {
 
     public List<PantryItem> updatePantryItemsAmount(List<UpdatePantryItemDTO> pantryItems) {
         List<PantryItem> updatedItems = new ArrayList<>();
+
         for(UpdatePantryItemDTO itemUpdate : pantryItems) {
             PantryItem item = pantryItemRepository.findById(itemUpdate.pantryItemId()).orElse(null);
+
             if(item != null) {
                 item.setAmount(itemUpdate.pantryAmount());
+
                 if(item.getAmount() == 0) {
                     item.setIsActive(false);
                 }
+
                 updatedItems.add(item);
             }
         }
+
         pantryItemRepository.saveAll(updatedItems);
+
         return updatedItems;
     }
 
     public PantryItemDetailsDTO getPantryItemDetails(Long pantryItemId) {
         return pantryItemRepository.findPantryItemDetailsById(pantryItemId)
-                .orElseThrow(() -> new PantryItemNotFoundException("No pantry item matching the given id"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No pantry item matching the given id"));
     }
 }
