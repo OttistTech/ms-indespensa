349577b46bc58adca0db519d146ede5b003d1f33
pedroggwp <phsnnascimento@outlook.com>
2024-09-18 09:10:18 -0300

    removing unsed exceptions handlers

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/exception/GlobalExceptionHandler.java
@@ -41,15 +41,6 @@ public class GlobalExceptionHandler {
         return problemDetail;
     }
 
-    @ExceptionHandler(UserNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ProblemDetail handleUserNotFoundException(UserNotFoundException ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-        problemDetail.setTitle("User not found");
-
-        return problemDetail;
-    }
-
     @ExceptionHandler(IncorrectPasswordException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ProblemDetail handleIncorrectPasswordException(IncorrectPasswordException ex) {
@@ -68,47 +59,11 @@ public class GlobalExceptionHandler {
         return problemDetail;
     }
 
-    @ExceptionHandler(AddressNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ProblemDetail handleUserAddressNotFound(AddressNotFoundException ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-        problemDetail.setTitle("Address not found with this user");
-
-        return problemDetail;
-    }
-
-    @ExceptionHandler(EanCodeNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ProblemDetail handleEanCodeNotFoundException(EanCodeNotFoundException ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-        problemDetail.setTitle("EAN code not found");
-
-        return problemDetail;
-    }
-
     @ExceptionHandler(JsonParcealizationException.class)
     @ResponseStatus(HttpStatus.BAD_GATEWAY)
     public ProblemDetail handleJsonParcealizationException(JsonParcealizationException ex) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_GATEWAY, ex.getMessage());
         problemDetail.setTitle("Json Parcealization Exception");
-
-        return problemDetail;
-    }
-
-    @ExceptionHandler(CategoryNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ProblemDetail handleCategoryNotFoundException(CategoryNotFoundException ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-        problemDetail.setTitle("Category not found");
-
-        return problemDetail;
-    }
-
-    @ExceptionHandler(PantryItemNotFoundException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ProblemDetail handlePantryItemNotFoundException(PantryItemNotFoundException ex) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
-        problemDetail.setTitle("Pantry item not found");
 
         return problemDetail;
     }
