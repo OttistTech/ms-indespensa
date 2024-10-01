dea4f42390821fa05c0d80979cdbaa29a1474725
pedroggwp <phsnnascimento@outlook.com>
2024-10-01 07:13:48 -0300

    controller of Recipe Entity

---
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/controller/RecipeController.java
@@ -0,0 +1,28 @@
+package com.ottistech.indespensa.api.ms_indespensa.controller;
+
+import com.ottistech.indespensa.api.ms_indespensa.dto.request.CreateRecipeDTO;
+import com.ottistech.indespensa.api.ms_indespensa.dto.response.RecipeFullInfoResponseDTO;
+import com.ottistech.indespensa.api.ms_indespensa.service.RecipeService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/recipe")
+@AllArgsConstructor
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @PostMapping("/create")
+    public ResponseEntity<RecipeFullInfoResponseDTO> createRecipe(@RequestBody @Valid CreateRecipeDTO recipeDTO) {
+
+        RecipeFullInfoResponseDTO recipe = recipeService.createRecipe(recipeDTO);
+
+        return ResponseEntity.ok(recipe);
+    }
+}
