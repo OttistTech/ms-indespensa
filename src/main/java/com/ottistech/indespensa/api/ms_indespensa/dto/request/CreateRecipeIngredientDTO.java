c195a973afeb07d0a1c4618af6af3cf9115bcd0d
pedroggwp <phsnnascimento@outlook.com>
2024-10-01 07:58:38 -0300

    changing fields amount and unit to be not null

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/dto/request/CreateRecipeIngredientDTO.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/dto/request/CreateRecipeIngredientDTO.java
@@ -7,8 +7,8 @@ import java.math.BigDecimal;
 
 public record CreateRecipeIngredientDTO(
         @NotNull(message = "Field foodId is required") Long foodId,
-        @NotNull(message = "Field amount is required") @Digits(integer = 10, fraction = 2) BigDecimal amount,
-        @NotNull(message = "Field unit is required") String unit,
+        @Digits(integer = 10, fraction = 2) BigDecimal amount,
+        String unit,
         @NotNull(message = "Field isEssential is required") Boolean isEssential
 ) {
 }
