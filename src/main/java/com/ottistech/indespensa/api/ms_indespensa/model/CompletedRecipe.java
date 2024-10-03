6e895b413ead91ede9a7fa4038be024764ae9a39
pedroggwp <phsnnascimento@outlook.com>
2024-10-03 12:26:08 -0300

    adding new constructor

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/model/CompletedRecipe.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/model/CompletedRecipe.java
@@ -1,6 +1,8 @@
 package com.ottistech.indespensa.api.ms_indespensa.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,14 +10,13 @@ import lombok.Setter;
 import org.hibernate.annotations.Check;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "completed_recipes")
+@Table(name = "completed_recipes", uniqueConstraints = { @UniqueConstraint(columnNames = {"user_id", "recipe_id"})})
 public class CompletedRecipe {
 
     @Id
@@ -35,6 +36,15 @@ public class CompletedRecipe {
     private LocalDateTime date = LocalDateTime.now();
 
     @Column(name = "num_stars", nullable = false)
+    @Min(value = 0, message = "Field numStars must be 0 => numStars <= 5")
+    @Max(value = 5, message = "Field numStars must be 0 => numStars <= 5")
     @Check(constraints = "num_stars >= 1 AND num_stars <= 5")
     private Integer numStars;
+
+    public CompletedRecipe(User user, Recipe recipe, Integer numStars) {
+        this.user = user;
+        this.recipe = recipe;
+        this.numStars = numStars;
+        this.date = LocalDateTime.now();
+    }
 }
