4aca9737803b93f68d43e76cf45cb8394470665b
pedroggwp <phsnnascimento@outlook.com>
2024-10-17 10:33:49 -0300

    endpoint to get the profile info, such as: itemsInPantryCount, purchasesMadeCount, productsAlreadyExpiredCount and recipesMadeCount

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/controller/DashboardController.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/controller/DashboardController.java
@@ -1,6 +1,7 @@
 package com.ottistech.indespensa.api.ms_indespensa.controller;
 
 import com.ottistech.indespensa.api.ms_indespensa.dto.response.DashboardInfoDTO;
+import com.ottistech.indespensa.api.ms_indespensa.dto.response.DashboardProfileInfoDTO;
 import com.ottistech.indespensa.api.ms_indespensa.service.DashboardService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +18,18 @@ public class DashboardController {
     private final DashboardService dashboardService;
 
     @GetMapping("/{user_id}/personal")
-    public ResponseEntity<?> getDashInfo(@PathVariable("user_id") Long userId) {
+    public ResponseEntity<DashboardInfoDTO> getDashInfo(@PathVariable("user_id") Long userId) {
 
         DashboardInfoDTO dashboardInfoDTO = dashboardService.getPantryItemDashInfo(userId);
 
         return ResponseEntity.ok(dashboardInfoDTO);
+    }
+
+    @GetMapping("/{user_id}/profile")
+    public ResponseEntity<?> getProfileInfo(@PathVariable("user_id") Long userId) {
+
+        DashboardProfileInfoDTO dashboardProfileInfoDTO = dashboardService.getProfileDashInfo(userId);
+
+        return ResponseEntity.ok(dashboardProfileInfoDTO);
     }
 }
