3612a7db3b995232443747975b1410dc87176e0a
pedroggwp <phsnnascimento@outlook.com>
2024-10-17 10:35:26 -0300

    service method to return the DashboardProfileInfoDTO with some queries

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/service/DashboardService.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/service/DashboardService.java
@@ -1,8 +1,11 @@
 package com.ottistech.indespensa.api.ms_indespensa.service;
 
 import com.ottistech.indespensa.api.ms_indespensa.dto.response.DashboardInfoDTO;
+import com.ottistech.indespensa.api.ms_indespensa.dto.response.DashboardProfileInfoDTO;
 import com.ottistech.indespensa.api.ms_indespensa.model.User;
+import com.ottistech.indespensa.api.ms_indespensa.repository.CompletedRecipeRepository;
 import com.ottistech.indespensa.api.ms_indespensa.repository.PantryItemRepository;
+import com.ottistech.indespensa.api.ms_indespensa.repository.ShopItemRepository;
 import com.ottistech.indespensa.api.ms_indespensa.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,21 +19,29 @@ import java.time.LocalDate;
 public class DashboardService {
     private final UserRepository userRepository;
     private final PantryItemRepository pantryItemRepository;
+    private final ShopItemRepository shopItemRepository;
+    private final CompletedRecipeRepository completedRecipeRepository;
 
     public DashboardInfoDTO getPantryItemDashInfo(Long userId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist"));
 
-        Integer itemCount = pantryItemRepository.countAllActiveItemsByUser(user);
+        Integer itemsInPantryCount = pantryItemRepository.countAllActiveItemsByUser(user);
         LocalDate lastPurchaseDate = pantryItemRepository.getLastPurchaseDate(user);
         int daysFromNow = 3;
         Integer itemsCloseToExpirationDateCount = pantryItemRepository.countAllItemsWithValidityWithinNextProvidedDays(user, LocalDate.now(), LocalDate.now().plusDays(daysFromNow));
         Integer possibleRecipes = pantryItemRepository.countAllPossibleRecipes(user.getUserId());
 
-        return new DashboardInfoDTO(
-                itemCount,
-                lastPurchaseDate,
-                itemsCloseToExpirationDateCount,
-                possibleRecipes
-        );
+        return DashboardInfoDTO.fromAllDetails(itemsInPantryCount, lastPurchaseDate, itemsCloseToExpirationDateCount, possibleRecipes);
+    }
+
+    public DashboardProfileInfoDTO getProfileDashInfo(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User does not exist"));
+
+        Integer itemsInPantryCount = pantryItemRepository.countAllActiveItemsByUser(user);
+        Integer purchasesMadeCount = shopItemRepository.countAllPurchaseHistoryItemsByUser(user);
+        Integer productsAlreadyExpired = pantryItemRepository.countAllItemsAlreadyExpired(user, LocalDate.now());
+        Integer recipesMadeCount = completedRecipeRepository.countAllCompletedRecipesByUser(user);
+
+        return DashboardProfileInfoDTO.fromAllDetails(itemsInPantryCount, purchasesMadeCount, productsAlreadyExpired, recipesMadeCount);
     }
 }
