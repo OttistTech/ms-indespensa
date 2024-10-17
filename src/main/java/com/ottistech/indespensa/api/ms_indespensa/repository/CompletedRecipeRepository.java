3c6588b12d9e148ebbd869a4f59e1c9d215431ee
pedroggwp <phsnnascimento@outlook.com>
2024-10-17 10:32:56 -0300

    query to return the amount of completed recipes by user

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/repository/CompletedRecipeRepository.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/repository/CompletedRecipeRepository.java
@@ -4,10 +4,19 @@ import com.ottistech.indespensa.api.ms_indespensa.model.CompletedRecipe;
 import com.ottistech.indespensa.api.ms_indespensa.model.Recipe;
 import com.ottistech.indespensa.api.ms_indespensa.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface CompletedRecipeRepository extends JpaRepository<CompletedRecipe, Long> {
 
     Optional<CompletedRecipe> findByUserAndRecipe(User user, Recipe recipe);
+
+    @Query("""
+    SELECT
+        COUNT(*)
+    FROM CompletedRecipe cr
+    WHERE cr.user = :user
+    """)
+    Integer countAllCompletedRecipesByUser(User user);
 }
