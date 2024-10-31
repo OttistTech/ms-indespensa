68e1a4eaebf1c340f0c49adfed034a7e850719eb
pedroggwp <phsnnascimento@outlook.com>
2024-10-30 21:07:00 -0300

    testing security

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/controller/UserController.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/controller/UserController.java
@@ -6,12 +6,15 @@ import com.ottistech.indespensa.api.ms_indespensa.dto.user.request.SignUpUserDTO
 import com.ottistech.indespensa.api.ms_indespensa.dto.user.request.UpdateUserDTO;
 import com.ottistech.indespensa.api.ms_indespensa.dto.user.response.UserCredentialsResponseDTO;
 import com.ottistech.indespensa.api.ms_indespensa.dto.user.response.UserFullInfoResponseDTO;
+import com.ottistech.indespensa.api.ms_indespensa.model.User;
 import com.ottistech.indespensa.api.ms_indespensa.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -66,9 +69,6 @@ public class UserController implements UserContract {
             @RequestParam("full-info")
             boolean fullInfo
     ) {
-
-        // verify if user_id passed is the same of the email in repository
-
         if (fullInfo) {
             UserFullInfoResponseDTO userFullInfo = userService.getUserFullInfo(userId);
             return ResponseEntity.ok(userFullInfo);
