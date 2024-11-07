3b81403f6f9c15ca46d6971cdb6a1e11b5d1f899
pedroggwp <phsnnascimento@outlook.com>
2024-11-07 09:16:43 -0300

    returning as the fields in database

--- src/main/java/com/ottistech/indespensa/api/ms_indespensa/dto/cep/response/CepApiResponse.java
+++ src/main/java/com/ottistech/indespensa/api/ms_indespensa/dto/cep/response/CepApiResponse.java
@@ -1,9 +1,19 @@
 package com.ottistech.indespensa.api.ms_indespensa.dto.cep.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record CepApiResponse(
-        String cep, // cep_id
-        String logradouro, // street
-        String uf, // state
-        String localidade // city
+
+        @JsonProperty("cep_id")
+        String cep,
+
+        @JsonProperty("street")
+        String logradouro,
+
+        @JsonProperty("state")
+        String uf,
+
+        @JsonProperty("city")
+        String localidade
 ) {
 }
